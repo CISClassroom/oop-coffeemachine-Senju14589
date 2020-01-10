cc9554d7527b47d2022cd47390da61dee9a15f30
Senju14589 <59078952+Senju14589@users.noreply.github.com>
2020-01-10 16:29:03 +0700

    lab 03 623410008-8
    เมนูกาแฟ
    1.Americano

---
+++ src/com/company/CoffeeMachine.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    int coffee = 10;
+    int water = 10;
+    public void  MakeCoffee(int iAmericano){
+        if (iAmericano >0){
+            for (int i = 0; i < iAmericano; i++){
+                Americano americano = MakeAmericano();
+                if ( americano !=null )
+                    System.out.println(i + "cup, Here your americano");
+                else
+                    System.out.println("Sorry can't make this cup.");
+
+            }
+        }
+        System.out.println("Select your Coffee");
+        System.out.println("1. Americano");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        if (input == "1") {
+            Americano americano = MakeAmericano();
+            if (americano != null)
+                System.out.println("Her your americano");
+            else
+                System.out.println("Sorry can't make it.");
+        }
+    }
+    public Americano MakeAmericano(){
+        //check recipe = americano offee 1 , water 2
+        if (coffee > 1 && water > 2) {
+            coffee -= 1;
+            water -= 2;
+            //creat americano object
+            Americano americano = new Americano();
+            //return americano object
+            return americano;
+        } else
+            return null;
+    }
+}
+
